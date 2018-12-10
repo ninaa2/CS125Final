0fed6a34cb62205ec1142b8dce53d7d1a6b8fa2d
ninaa2 <42974778+ninaa2@users.noreply.github.com>
2018-12-09 20:52:20 -0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,11 @@
 package com.example.radhikasriram.cs125final;
 
 import android.content.Intent;
-import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     final String apple = "AAPL";
@@ -102,78 +93,6 @@ public class MainActivity extends AppCompatActivity {
         b.putString("key", symbol);
         myIntent.putExtras(b);
         startActivity(myIntent);
-    }
-    private static String convert(InputStream a) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(a));
-        StringBuilder s = new StringBuilder();
-
-        String input = null;
-        try {
-            while ((input = reader.readLine()) != null) {
-                s.append(input + "\n");
-
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                a.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return s.toString();
-    }
-
-    public static String[] connect(String url) {
-        HttpClient httpClient = new DefaultHttpClient();
-        String[] newString = new String[5];
-
-        Httpget httpget = new HttpGet(url);
-        HttpResponse response;
-        try {
-            response = httpClient.execute(httpget);
-            Log.d(Tag, response.getStatusLine().toString());
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                InputStream instream = entity.getContent();
-                String result = convertStreamToString(instream);
-                result = result.replace("parseExchangeRate(", "").replace("); ", "");
-                Log.d(Tag, result);
-
-                JSONObject json = new JSONObject(result);
-                Log.d(Tag,"<jsonobject>\n" + json.toString() + "\n</jsonobject>");
-                JSONObject query = json.getJSONObject("query");
-                Log.d(Tag, query.toString());
-                JSONObject results = query.getJSONObject("results");
-                Log.d(Tag, results.toString());
-                JSONObject quote = results.getJSONObject("row");
-                Log.d(Tag, quote.toString());
-                for (int i = 0; i < quote.length(); i++) {
-                    String symbol = quote.getString("rate");
-                    newString[0] = symbol;
-                    String dayslow = quote.getString("DaysLow");
-                    newString[1]=dayslow;
-                    // tv1.setText(quote.getString("DaysLow"));
-                    newString[2]= quote.getString("DaysHigh");
-                    newString[3]= quote.getString("Open");
-                    newString[4]= quote.getString("Change");
-                }
-                Log.d(TAG, "<jsonobject>\n" + json.toString() + "\n</jsonobject>");
-                instream.close();
-            }
-        } catch (ClientProtocolException e) {
-            Log.d(Tag, "ClientProtocolException");
-            e.printStackTrace();
-        } catch (IOException e) {
-            Log.d(Tag, "IOException " + e.getMessage());
-            e.printStackTrace();
-        } catch (JSONException e) {
-            Log.d(Tag, "JSONException " + e.getMessage());
-            e.getMessage();
-        }
-        return newString;
     }
 
 }
